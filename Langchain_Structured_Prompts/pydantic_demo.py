b82ed0fd840b43a19c691663350d3ce021cc9101
icodebest <waleedusmaneng12@gmail.com>
2025-04-29 23:29:20 +0500

    Pydantic demo

--- Langchain_Structured_Prompts/pydantic_demo.py
+++ Langchain_Structured_Prompts/pydantic_demo.py
@@ -1,2 +1,14 @@
-from pydantic import BaseModel
-cl
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class Student(BaseModel):
+    name: str= 'Waleed'
+    age:Optional[int] = None
+    email: EmailStr
+    cgpa: float=Field(default=0.0, gt=0.0, le=4.0, description="CGPA must be between 0 and 4")
+
+new_student={'age':20, 'email':'abc@gmail.com', 'cgpa':3.45}
+
+student = Student(**new_student)
+
+print(student)
