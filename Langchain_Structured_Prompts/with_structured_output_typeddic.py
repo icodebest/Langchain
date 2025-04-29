c986d943380f5fda2e9f084e28f8914e4c76ca4f
icodebest <waleedusmaneng12@gmail.com>
2025-04-29 18:41:16 +0500

    Pydantic in Python

--- Langchain_Structured_Prompts/with_structured_output_typeddic.py
+++ Langchain_Structured_Prompts/with_structured_output_typeddic.py
@@ -24,4 +24,4 @@ result = structured_model.invoke("""
 I’ve had this smart vacuum cleaner for about two months now, and I have mixed feelings. On the plus side, it navigates very well around furniture, has strong suction for its size, and does a solid job with pet hair—something my older vacuum constantly struggled with. The companion app is intuitive, and scheduling cleanings is a breeze. But the battery life is disappointing—it barely makes it through a full session in my apartment, which isn’t huge. It also tends to get stuck under my sofa unless I physically block it. Customer support was helpful, but it took three days to get a response. I appreciate the features, but for this price point, I expected more polish. If battery and navigation quirks were fixed, I’d be 100% on board.
 """)
 
-print(result)
+print(result['sentiment'])
